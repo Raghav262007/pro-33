da32d62e516adb64c9343c3d26bd91295ba6651d
Raghav262007 <77057843+Raghav262007@users.noreply.github.com>
2021-05-09 18:00:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+
+var engine, world;
+
+var backgroundImg;
+var snowmanImg, snowmanImg2;
+var girlImg, girlSprite;
+
+function preload() {
+  backgroundImg = loadImage("snow2.jpg");
+  snowmanImg = loadImage("snowman img.png");
+  snowmanImg2 = loadImage("snowman img 2.png");
+  girlImg = loadImage("girl img.png");
+}
+
+function setup() {
+  createCanvas(800, 400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  var snowman = createSprite(730, 320, 50, 50);
+  snowman.addImage("snowman", snowmanImg);
+  snowman.scale = 0.3;
+
+  var snowman2 = createSprite(100, 320, 50, 50);
+  snowman2.addImage("snowman2", snowmanImg2);
+  snowman2.scale = 0.3;
+
+  girlSprite = createSprite(480, 320, 50, 50);
+  girlSprite.addImage("girl", girlImg);
+  girlSprite.scale = 0.3;
+}
+
+function draw() {
+  background(backgroundImg);
+  Engine.update(engine);
+
+  keypressed();
+  drawSprites();
+}
+
+function keypressed() {
+  if (keyCode === 39) {
+    girlSprite.x = girlSprite.x + 2;
+  } else if (keyCode === 37) {
+    girlSprite.x = girlSprite.x - 2;
+  }
+}
